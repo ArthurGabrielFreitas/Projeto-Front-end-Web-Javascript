22d02470380f413f71f83ed5a07e7897cfc0850f
Arthur Gabriel de Freitas Dantas Morais <artgabrielfdm@gmail.com>
2026-06-02 22:22:22 -0300

    Aula sobre form e email via react

--- app01/src/App.jsx
+++ app01/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Main from "./main/Main";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
+import Formulario from "./componentes/Formulario/Formulario"
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Formulario />
       <Footer />
     </>
   );
---
+++ app01/src/componentes/Formulario/Formulario.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import style from './Formulario.module.css'
+import emailjs from '@emailjs/browser';
+
+function Formulario() {
+
+  const [form, setForm] = useState({
+    nome: '',
+    email: '',
+    msg: ''
+  });
+
+  const [msgErro, setMsgErro] = useState("");
+
+  function alterarForm(e) {
+    setForm({...form, [e.target.name]: e.target.value})
+
+    if (form.nome != '' && form.email != '' && form.msg != '') {
+      setMsgErro("")
+    }
+  }
+
+  function validar(e) {
+    e.preventDefault();
+
+    if (form.nome == '' || form.email == '' || msg == '') {
+      setMsgErro("Todos os campos devem ser preenchidos")
+    } else {
+      var templateParams = {
+        from_name: form.nome,
+        from_email: form.email,
+        from_msg: form.msg
+      };
+      
+      emailjs.send('service_bs51g31', 'template_ivlds75', templateParams, 'PsBTHZHTU9WuU9pot').then(
+        (response) => {
+          setMsgErro("Email enviado com sucesso");
+        },
+        (error) => {
+          setMsgErro("Não foi possível enviar o e-mail");
+        },
+      );
+    }
+  }
+
+  return (
+    <>
+        <form className={style.form} onSubmit={validar}>
+            <label htmlFor="nome">Nome</label>
+            <input type="text" id='nome' name='nome' onChange={alterarForm}/>
+            
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' name='email' onChange={alterarForm}/>
+            
+            <label htmlFor="mensagem">Mensagem</label>
+            <textarea id='msg' name='msg' onChange={alterarForm}/>
+
+            <button type='submit'>Enviar</button>
+            <p>{msgErro}</p>
+        </form>
+    </>
+  )
+}
+
+export default Formulario
